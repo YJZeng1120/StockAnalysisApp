dd97aaf63c6a7ce67a37a7fbb54b5294d2459829
crystal <crystal.zeng@slc.com.tw>
2026-03-05 09:33:47 +0800

    Initial Commit

---
+++ StockAnalysisApp/StockAnalysisAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockAnalysisAppApp.swift
+//  StockAnalysisApp
+//
+//  Created by crystal.zeng on 2026/3/5.
+//
+
+import SwiftUI
+
+@main
+struct StockAnalysisAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
